945847ed2afdbedbb0200d2556f4356bc97dc5ff
buenoIago <iagocedup@gmail.com>
2026-03-09 15:30:32 -0300

    adicionado geração de números aleatórios

--- jogoDeAdivinhacao.Console.App/Program.cs
+++ jogoDeAdivinhacao.Console.App/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+//biblioteca padrão do sistema "relacionado a criptografia".
+using System.Security.Cryptography;
+
+while (true)
+{
+
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Bem Vindo ao Jogo de Adivinhação! ");
+Console.WriteLine("----------------------------------------");  
+
+//Random.Shared.Next(1, 21); pode ser usado, mas o RandomNumberGenerator é mais seguro.
+
+int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+
+Console.WriteLine("Digite um número entre 1 e 20: ");
+string chute = Console.ReadLine();
+
+Console.WriteLine($"O valor digitado foi: {chute}");
+Console.WriteLine($"O número aleatório era: {numeroAleatorio}");
+Console.ReadLine();
+
+}
